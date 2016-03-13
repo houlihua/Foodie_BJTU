1728d08e15e220e1e25efa4324734f1720d7aaaa
chendongmidong <445397734@qq.com>
2016-03-14 00:30:11 +0800

    MainActivity UI

--- android/src/com/example/FoodieAndroidApp/activity/MainActivity.java
+++ android/src/com/example/FoodieAndroidApp/activity/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.FoodieAndroidApp.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import com.astuetz.PagerSlidingTabStrip;
 import com.example.FoodieAndroidApp.R;
+import com.example.FoodieAndroidApp.fragment.SuperAwesomeCardFragment;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends FragmentActivity {
+
+    private com.astuetz.PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+    private MyPagerAdapter adapter;
+    private DisplayMetrics dm;
     /**
      * Called when the activity is first created.
      */
@@ -16,6 +32,39 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        dm = getResources().getDisplayMetrics();
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        pager = (ViewPager) findViewById(R.id.pager);
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        tabs.setViewPager(pager);
+        setTabsValue();
+    }
+
+    private void setTabsValue() {
+        // 设置Tab是自动填充满屏幕的
+        tabs.setShouldExpand(true);
+        // 设置Tab的分割线是透明的
+        tabs.setDividerColor(Color.TRANSPARENT);
+
+
+
+        // 设置Tab底部线的高度
+        tabs.setUnderlineHeight((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 1, dm));
+        // 设置Tab Indicator的高度
+        tabs.setIndicatorHeight((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 4, dm));
+        // 设置Tab标题文字的大小
+        tabs.setTextSize((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP, 16, dm));
+        // 设置Tab Indicator的颜色
+        tabs.setIndicatorColor(Color.parseColor("#45c01a"));
+        // 设置选中Tab文字的颜色 (这是我自定义的一个方法)
+        tabs.setSelectedTextColor(Color.parseColor("#45c01a"));
+        // 取消点击Tab时的背景色
+        tabs.setTabBackground(0);
     }
 
     @Override
@@ -32,5 +81,30 @@ public class MainActivity extends Activity {
                 item.setIntent(new Intent(this,FriendList.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = { "Fragment1", "Fragment2", "Fragment3", "Fragment4"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return SuperAwesomeCardFragment.newInstance(position);
+        }
+
     }
 }
